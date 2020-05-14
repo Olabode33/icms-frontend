1a684c32b0103d4f1be712a1b82793164fd08884
olabode33 <olabode33@gmail.com>
2020-05-14 09:47:28 +0100

    fix dashboard url

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component, Injector, ViewEncapsulation } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { DashboardCustomizationConst } from '@app/shared/common/customizable-dashboard/DashboardCustomizationConsts';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
 
 @Component({
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.less'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    animations: [appModuleAnimation()]
 })
 
 export class DashboardComponent extends AppComponentBase {
